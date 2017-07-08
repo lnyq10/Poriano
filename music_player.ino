1461863a85e0a9dde059981c95f6b2de1b42388a
lnyq10 <lnyq10@sjtu.edu.cn>
2017-07-08 21:06:27 +0800

    Add files via upload

--- music_player.ino
+++ music_player.ino
@@ -1,33 +1,42 @@
-unsigned char JDQdata1[7]={0X7E ,0X02, 0X02 ,0XEF};  
-unsigned char Number2[7]={0X7e,0X04,0X41,0X00,0X08,0XEF};  
-unsigned char Number3[7]={0X7e,0X04,0X41,0X00,0X04,0XEF};   
-unsigned char JDQdata4[7]={0X7e,0X04,0X41,0X00,0X01,0Xef};  
+unsigned char StopPlay[7]={0X7E ,0X02, 0X02 ,0XEF};  //stop the speaker from playing sounds
+unsigned char Button1[7]={0X7e,0X04,0X41,0X00,0X01,0XEF};  //Play the first sound.
+unsigned char Button2[7]={0X7e,0X04,0X41,0X00,0X02,0XEF};  
+unsigned char Button3[7]={0X7e,0X04,0X41,0X00,0X03,0XEF};  
+unsigned char Button4[7]={0X7e,0X04,0X41,0X00,0X04,0XEF};  
+unsigned char Button5[7]={0X7e,0X04,0X41,0X00,0X05,0XEF};  
+unsigned char Button6[7]={0X7e,0X04,0X41,0X00,0X06,0XEF};  
+unsigned char Button7[7]={0X7e,0X04,0X41,0X00,0X07,0XEF};  
+unsigned char Button8[7]={0X7e,0X04,0X41,0X00,0X08,0XEF};  
+unsigned char Button9[7]={0X7e,0X04,0X41,0X00,0X09,0XEF};  
+unsigned char Button10[7]={0X7e,0X04,0X41,0X00,0X0A,0XEF};  //Play the tenth sound.
+ 
 
 void setup() {
   
   Serial.begin(9600);
   Serial1.begin(9600);
   Serial2.begin(9600);
+  Serial3.begin(9600);
 }
 
 void loop() {
   
   
   
+  //For test
+  Serial1.write(Button10,6);
+  Serial2.write(Button2,6);
   
-  Serial1.write(Number2,6);
-  Serial2.write(Number3,6);
-  Serial.println("i am here");
-  delay(1000);
-  
-  
-  Serial1.write(JDQdata1,4);
-  Serial2.write(JDQdata1,4);
+  delay(500);  
+  Serial1.write(StopPlay,4);
+  Serial2.write(StopPlay,4);
+
+
+
+
  
   delay(6000);
 }
-
-
 
 
 
